47abdbcd1bb808d19f44daf62b793717c0c76e8b
Johnny Li <johnkcli@hotmail.com>
2020-10-20 21:42:53 +0100

    fixed styling for buttons and id for list

--- src/components/AudioList.vue
+++ src/components/AudioList.vue
@@ -3,11 +3,7 @@
     <audio-recorder @recording-done="recordingDone" />
     <button @click="playNonstop" type="button">Play Nonstop</button>
     <draggable :list="list" :move="checkMove" class="list-group">
-      <div
-        class="list-group-item"
-        v-for="element in list"
-        :key="element.recording"
-      >
+      <div class="list-group-item" v-for="element in list" :key="element.id">
         {{ element.name }}
       </div>
     </draggable>
@@ -17,6 +13,7 @@
 <script>
 import draggable from "vuedraggable";
 import AudioRecorder from "./AudioRecorder";
+import uniqueId from "lodash.uniqueid";
 export default {
   name: "AudioList",
   components: {
@@ -34,13 +31,41 @@ export default {
     },
     recordingDone: function (recording) {
       let character = window.prompt("Enter character name", "Harry Potter");
-      this.list.push({ name: character, recording: recording });
+      this.list.push({
+        name: character,
+        recording: recording,
+        id: uniqueId("line-"),
+      });
     },
-    playNonstop: function (){
-        this.$emit('play-nonstop', this.list);
+    playNonstop: function () {
+      this.$emit("play-nonstop", this.list);
     },
   },
 };
 </script>
 <style scoped>
+button {
+  font-size: 18px;
+  font-weight: 200;
+  padding: 1em;
+  width: 200px;
+  background: transparent;
+  border: 4px solid #f22f46;
+  border-radius: 4px;
+  transition: all 0.4s ease 0s;
+  cursor: pointer;
+  color: #f22f46;
+  margin-bottom: 4em;
+  margin: 0;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+}
+button:hover,
+button:focus {
+  background: #f22f46;
+  color: #fff;
+}
 </style>
