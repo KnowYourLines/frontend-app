5da1936c078efd41f6a9e5ad238ec540ba8a7043
Johnny Li <johnkcli@hotmail.com>
2020-10-22 03:59:19 +0100

    name editting working

--- src/components/AudioList.vue
+++ src/components/AudioList.vue
@@ -4,23 +4,33 @@
     <button class="play" @click="playNonstop" type="button">
       Play Nonstop
     </button>
+    <button
+      class="edit"
+      @click="toggleToItemEditForm"
+      v-if="!isEditing"
+      type="button"
+    >
+      Edit
+    </button>
+    <div class="btn-group" v-else>
+      <button type="button" class="edit" @click="editDone">Done</button>
+    </div>
     <draggable :list="list" :move="checkMove" class="list-group">
       <div class="list-group-item" v-for="element in list" :key="element.id">
         {{ element.name }}
         <div class="btn-group" v-if="!isEditing">
-          <button type="button" class="btn" @click="toggleToItemEditForm">Edit</button>
           <button type="button" class="btn" @click="deleteLine(element.id)">
             Delete
           </button>
         </div>
-        <audio-edit
-          v-else
-          :id="element.id"
-          :name="element.name"
-          @item-edited="itemEdited"
-          @edit-cancelled="editCancelled"
-          >></audio-edit
-        >
+        <div v-else>
+          <label class="edit-label">Change name: </label>
+          <input
+            type="text"
+            autocomplete="on"
+            v-model.lazy.trim="element.name"
+          />
+        </div>
       </div>
     </draggable>
   </div>
@@ -29,19 +39,18 @@
 <script>
 import draggable from "vuedraggable";
 import AudioRecorder from "./AudioRecorder";
-import AudioEdit from "./AudioEdit";
 import uniqueId from "lodash.uniqueid";
 export default {
   name: "AudioList",
   components: {
     draggable,
     AudioRecorder,
-    AudioEdit
   },
   data() {
     return {
       list: [],
       isEditing: false,
+      newName: "",
     };
   },
   methods: {
@@ -65,14 +74,7 @@ export default {
     toggleToItemEditForm() {
       this.isEditing = true;
     },
-    editCancelled() {
-      this.isEditing = false;
-    },
-    itemEdited(newName, lineId) {
-      const lineIndex = this.list.findIndex((line) => line.id === lineId);
-      let line = this.list[lineIndex]
-      line['name'] = newName;
-      this.list.splice(lineIndex, 1, line);
+    editDone() {
       this.isEditing = false;
     },
   },
@@ -80,6 +82,25 @@ export default {
 </script>
 <style scoped>
 button.play {
+  font-size: 18px;
+  font-weight: 200;
+  padding: 1em;
+  width: 200px;
+  background: transparent;
+  border: 4px solid #2257ca;
+  border-radius: 4px;
+  transition: all 0.4s ease 0s;
+  cursor: pointer;
+  color: #2257ca;
+  margin-bottom: 4em;
+  margin: 0;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+}
+button.edit {
   font-size: 18px;
   font-weight: 200;
   padding: 1em;
--- src/components/AudioRecorder.vue
+++ src/components/AudioRecorder.vue
@@ -12,7 +12,7 @@
 export default {
   name: "AudioRecorder",
   data() {
-    return { isRecording: false, character: "Harry Potter" };
+    return { isRecording: false, character: "Character name" };
   },
   methods: {
     recordAudio() {
