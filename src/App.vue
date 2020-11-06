5165fc820e85b533532426d4c9fd1afac69b8225
Johnny Li <johnkcli@hotmail.com>
2020-11-06 23:33:07 +0000

    remove tdd and tutorial for now

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
   <div>
-    <new-message-form @send="addMessage" />
-    <message-list :messages="messages" />
     <audio-player/>
   </div>
 </template>
 
 <script>
-import NewMessageForm from './components/NewMessageForm';
-import MessageList from './components/MessageList';
 import AudioPlayer from './components/AudioPlayer';
 
 export default {
   name: 'App',
   components: {
-    NewMessageForm,
-    MessageList,
     AudioPlayer,
-  },
-  data() {
-    return {
-      messages: [],
-    };
-  },
-  methods: {
-    addMessage(text) {
-      this.messages.unshift(text);
-    }
   },
 };
 </script>
